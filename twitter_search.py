c120fda863a8dabf31124ac5c25d577830bb0524
ujicaesar <fcruji@gmail.com>
2019-02-21 15:52:07 +0700

    Get API Twitter

---
+++ twitter_search.py
@@ -0,0 +1,45 @@
+# Import the necessary package to process data in JSON format
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+# Import the twitter library
+from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
+
+# Variables that contains the user credentials to access Twitter API 
+ACCESS_TOKEN = '219318879-VO2z6J7qIK4hjKLAz9E68aKEeHDRoeJwIo1zdxMz'
+ACCESS_SECRET = 'skadu9quTk94npgW6xKFxXH42YcbiWe3rKtgKMDNUmVA3'
+CONSUMER_KEY = 'brLTTDqGomjHKRXFnu4JkjJID'
+CONSUMER_SECRET = 'PdEklANwDdCuIVc3i6J1J0LEa1kvZqy5UvBgYGLOoKGGyHzjIl'
+
+# Setup twitter to authenticate with Twitter credentials:
+
+oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+
+twitter = Twitter(auth=oauth)
+
+# Create the api to connect to twitter with your creadentials
+
+tweets = twitter.search.tweets(q='love', result_type='recent', lang='en', count=10)
+
+print(json.dumps(tweets, indent=4))
+
+#---------------------------------------------------------------------------------------------------------------------
+# wait_on_rate_limit= True;  will make the api to automatically wait for rate limits to replenish
+# wait_on_rate_limit_notify= Ture;  will make the api  to print a notification when Tweepyis waiting for rate limits to replenish
+#---------------------------------------------------------------------------------------------------------------------
+
+
+#---------------------------------------------------------------------------------------------------------------------
+# The following loop will print most recent statuses, including retweets, posted by the authenticating user and that user’s friends. 
+# This is the equivalent of /timeline/home on the Web.
+#---------------------------------------------------------------------------------------------------------------------
+
+# for status in tweepy.Cursor(api.home_timeline).items(200):
+# 	print(status._json)
+	
+#---------------------------------------------------------------------------------------------------------------------
+# Twitter API development use pagination for Iterating through timelines, user lists, direct messages, etc. 
+# To help make pagination easier and Tweepy has the Cursor object.
+#---------------------------------------------------------------------------------------------------------------------
---
+++ twitter_streaming.py
@@ -0,0 +1,60 @@
+# Import the necessary package to process data in JSON format
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+# Import the twitter library
+from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
+
+# Variables that contains the user credentials to access Twitter API 
+ACCESS_TOKEN = '219318879-VO2z6J7qIK4hjKLAz9E68aKEeHDRoeJwIo1zdxMz'
+ACCESS_SECRET = 'skadu9quTk94npgW6xKFxXH42YcbiWe3rKtgKMDNUmVA3'
+CONSUMER_KEY = 'brLTTDqGomjHKRXFnu4JkjJID'
+CONSUMER_SECRET = 'PdEklANwDdCuIVc3i6J1J0LEa1kvZqy5UvBgYGLOoKGGyHzjIl'
+
+# Setup twitter to authenticate with Twitter credentials:
+
+oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+
+twitter_stream = TwitterStream(auth=oauth)
+
+# Create the api to connect to twitter with your creadentials
+
+# iterator = twitter_stream.statuses.sample()
+iterator = twitter_stream.statuses.filter(track='samsung', language='en')
+
+tweet_count = 10
+
+print('[')
+for tweet in iterator:
+    tweet_count -= 1
+
+    print(json.dumps(tweet, indent=4), ',')
+
+    if tweet_count <= 0:
+        break
+
+
+print(']')
+
+
+
+#---------------------------------------------------------------------------------------------------------------------
+# wait_on_rate_limit= True;  will make the api to automatically wait for rate limits to replenish
+# wait_on_rate_limit_notify= Ture;  will make the api  to print a notification when Tweepyis waiting for rate limits to replenish
+#---------------------------------------------------------------------------------------------------------------------
+
+
+#---------------------------------------------------------------------------------------------------------------------
+# The following loop will print most recent statuses, including retweets, posted by the authenticating user and that user’s friends. 
+# This is the equivalent of /timeline/home on the Web.
+#---------------------------------------------------------------------------------------------------------------------
+
+# for status in tweepy.Cursor(api.home_timeline).items(200):
+# 	print(status._json)
+	
+#---------------------------------------------------------------------------------------------------------------------
+# Twitter API development use pagination for Iterating through timelines, user lists, direct messages, etc. 
+# To help make pagination easier and Tweepy has the Cursor object.
+#---------------------------------------------------------------------------------------------------------------------
